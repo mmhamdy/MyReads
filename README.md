a2ed3983bbd142a3b41e776e1a7bea917eb53126
mmhamdy <mohamdy2772@gmail.com>
2022-01-12 22:49:19 +0200

    creating the home route

---
+++ README.md
@@ -0,0 +1,1 @@
+## MyReads: A Book Tracking App
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BooksApp from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<BooksApp />, rootElement);
+
+
