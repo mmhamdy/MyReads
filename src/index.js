6c449554547a919fd83ec03b7590bbd5cc0e6447
mmhamdy <mohamdy2772@gmail.com>
2022-01-12 22:59:37 +0200

    adding BrowserRouter and Routes to index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import BooksApp from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<BooksApp />, rootElement);
+ReactDOM.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<BooksApp />} />
+        </Routes>
+    </BrowserRouter>, 
+    rootElement);
 
 
