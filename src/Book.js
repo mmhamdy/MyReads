9c174403ad9eb71cd99e4d51d5a835223106c448
mmhamdy <mohamdy2772@gmail.com>
2022-01-14 06:55:22 +0200

    converting BookPlacer into functional component and removing unnecessary imports

--- src/Book.js
+++ src/Book.js
@@ -1,4 +1,3 @@
-import React from 'react' 
 import BookPlacer from './BookPlacer'
 
 const Book = ({book, onChangeShelf}) => {
--- src/BookPlacer.js
+++ src/BookPlacer.js
@@ -1,25 +1,20 @@
-import React from 'react' 
-
-class BookPlacer extends React.Component {
-    book = this.props.book;
-    changeShelf = this.props.changeShelf;
-
-    moveBook = (e) => {
-        this.changeShelf(this.book, e.target.value);
+const BookPlacer = ({book, changeShelf}) => {
+    const moveBook = (e) => {
+        changeShelf(book, e.target.value);
     }
-    render() {
-        return(
-            <div className="book-shelf-changer">
-                <select value={this.props.book.shelf} onChange={this.moveBook}>
-                    <option value="move" disabled>Move to...</option>
-                    <option value="currentlyReading">Currently Reading</option>
-                    <option value="wantToRead">Want to Read</option>
-                    <option value="read">Read</option>
-                    <option value="none">None</option>
-                </select>
-            </div>
-        )
-    }
+
+    return(
+        <div className="book-shelf-changer">
+            <select value={book.shelf} onChange={moveBook}>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+            </select>
+        </div>
+    )
 }
+
 
 export default BookPlacer
