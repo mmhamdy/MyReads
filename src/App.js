76c93d91b0cbe79541e7e515491dd299e973b1cd
mmhamdy <mohamdy2772@gmail.com>
2022-01-15 02:11:18 +0200

    converting Shelf and AddBooks into functional components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import Shelf from './Shelf'
 import AddBooks from './AddBooks'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class Library extends React.Component {
+  state = {
+    books: [],
+  }
+
+  componentDidMount() {
+      this.populate();
+  }
+
+  populate = () => {
+      BooksAPI.getAll().then(books => this.setState({books}))
+  }
+
+  reposition = (book, shelf) => {
+      BooksAPI.update(book, shelf).then(result => this.populate())
+  }
+
   render() {
     return (
       <div className="app">
@@ -12,9 +28,9 @@ class Library extends React.Component {
           <div className="list-books-title">
             <h1>MyReads</h1>
           </div>
-          <Shelf  shelfTitle="Currently Reading" shelfKey="currentlyReading"/>
-          <Shelf  shelfTitle="Want to Read" shelfKey="wantToRead"/>
-          <Shelf  shelfTitle="Read" shelfKey="read"/>
+          <Shelf  shelfTitle="Currently Reading" shelfKey="currentlyReading" books={this.state.books} onChangeShelf={this.reposition}/>
+          <Shelf  shelfTitle="Want to Read" shelfKey="wantToRead" books={this.state.books} onChangeShelf={this.reposition}/>
+          <Shelf  shelfTitle="Read" shelfKey="read" books={this.state.books} onChangeShelf={this.reposition}/>
           <AddBooks />
         </div>
       </div>
--- src/Shelf.js
+++ src/Shelf.js
@@ -1,41 +1,18 @@
-import React from 'react'
 import Book from './Book'
-import * as BooksAPI from './BooksAPI'
 
-class Shelf extends React.Component {
-    state = {
-        books: [],
-    }
-
-    componentDidMount() {
-        BooksAPI.getAll().then(books => this.setState({books}));
-    }
-
-    populate = () => {
-        BooksAPI.getAll().then(books => this.setState({books}))
-    }
-
-    reposition = (book, shelf) => {
-        BooksAPI.update(book, shelf).then(result => this.populate())
-    }
-
-    shelfTitle = this.props.shelfTitle;
-    shelfKey = this.props.shelfKey;
-
-    render() {
-        return(
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.state.books.filter(book => book.shelf === this.props.shelfKey).map((book) => (
-                             <li key={book.id}><Book book={book} onChangeShelf={this.reposition} /></li>
-                        ))}
-                    </ol>
-                </div>
+const Shelf = ({shelfTitle, shelfKey, books, onChangeShelf}) => {
+    return(
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books.filter(book => book.shelf === shelfKey).map((book) => (
+                         <li key={book.id}><Book book={book} onChangeShelf={onChangeShelf} /></li>
+                    ))}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Shelf
